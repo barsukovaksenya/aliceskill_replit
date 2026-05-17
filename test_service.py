83508ed7a7aeda4725d589747a457916b5f0a215
barsukovaksenya <barsukovaksenya@gmail.com>
2026-05-17 15:22:01 +0300

    готово для replit

---
+++ test_service.py
@@ -0,0 +1,22 @@
+import requests
+
+
+url = ''
+
+data = {
+  "request": {
+    "command": "нет",
+    "original_utterance": "хорошо"
+  },
+  "session": {
+    "new": False,
+    "message_id": 1,
+    "session_id": "test-session-123",
+    "skill_id": "test-skill",
+    "user_id": "test-user-1"
+  },
+  "version": "1.0"
+}
+
+response = requests.post(url, json=data)
+print(response.json())
